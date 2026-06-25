faf2542638b8cf41f557369e5c8c6b5614d6ebc3
Gabor Sipoczy <gsipoczy@hotmail.com>
2026-06-25 14:46:54 +0200

    c15

---
+++ ai_hello.py
@@ -0,0 +1,20 @@
+fruits = [
+    "apple",
+    "banana",
+    "orange",
+    "mango",
+    "strawberry",
+    "pineapple",
+    "grape",
+    "watermelon",
+    "kiwi",
+    "peach",
+]
+
+
+def hello_world():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    hello_world()
