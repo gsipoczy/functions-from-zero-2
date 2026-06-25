12668ecd3ececed0f46ed141a9c571231db888d6
Gabor Sipoczy <gsipoczy@hotmail.com>
2026-06-25 11:58:14 +0200

    c11

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-x = 1
-y = 2
-print(x+y)
+import src.util as ut
+
+def add(x,y):
+    return x + y
+
+def multiply(x,y):
+    return ut.multiply(x,y)
 
---
+++ test_all.py
@@ -0,0 +1,5 @@
+import main
+import src.util as ut
+
+assert main.add(2,2) == 4
+assert ut.multiply(3,3) == 9
